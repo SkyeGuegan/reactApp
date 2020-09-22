3cc89de1123b8e6dc731202ef7c761a80565b922
SkyeGuegan <sguegan@pugetsound.edu>
2020-09-21 18:12:24 -0600

    Test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+// { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
@@ -13,10 +13,11 @@ function App() {
           Hiiiiiiiiiiiiiii
         </p>
       </header>
-      <AmplifySignOut />    
+      {//<AmplifySignOut />
+      }
     </div>
   );
 }
 
-//export default App;
-export default withAuthenticator(App);
+export default App;
+//export default withAuthenticator(App);
